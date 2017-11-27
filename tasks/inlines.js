3293ca172d2077b9ccdf2008cf4aee593dc5f3d6
sunxiaobo <sunxiaobo.cn@gmail.com>
2017-11-27 20:10:23 +0800

    first commit

---
+++ tasks/inlines.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var cssmin = require('cssmin')
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('inlines', 'inline sources to string', function() {
+    var options = this.options()
+    var cssminFlag = !!options.cssmin
+
+    this.files.forEach(function(obj) {
+      var src = obj.src[0]
+      var content = fs.readFileSync(src, { encoding: 'UTF-8'}) 
+
+      if (/styleCSS\s*:\s*__inline\(['"]([^'"]+)['"]\)/.test(content)) {
+        var name = RegExp.$1 
+        var filePath = path.join(path.dirname(src), name)
+
+        var fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' })
+
+        if (/\.css$/.test(filePath) && cssminFlag) {
+          fileContent = cssmin(fileContent) 
+        }
+
+        if (fs.existsSync(filePath)) {
+          fs.writeFileSync(src, content.replace(/__inline[^)]+\)/, `'${ fileContent }'`))
+        }
+      }
+    })
+  })
+}
