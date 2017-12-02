d0faa60b3475ed1c24efd7081587b965bd346d0e
sunxiaobo <sunxiaobo.cn@gmail.com>
2017-12-02 10:40:22 +0800

    v1.1.0 support inline html with html-minifier

--- tasks/inlines.js
+++ tasks/inlines.js
@@ -3,11 +3,22 @@
 var fs = require('fs')
 var path = require('path')
 var cssmin = require('cssmin')
+var htmlmin = require('html-minifier').minify
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('inlines', 'inline sources to string', function() {
     var options = this.options()
     var cssminFlag = !!options.cssmin
+    var htmlminFlag = !!options.htmlmin
+
+    var htmlminOptions = {
+      collapseInlineTagWhitespace: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      removeTagWhitespace: true
+    }
 
     this.files.forEach(function(obj) {
       var src = obj.src[0]
@@ -18,9 +29,20 @@ module.exports = function(grunt) {
         var filePath = path.join(path.dirname(src), name)
 
         var fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' })
+        var fileType = filePath.match(/\.([^/]+)$/)[1]
 
-        if (/\.css$/.test(filePath) && cssminFlag) {
-          fileContent = cssmin(fileContent) 
+        switch(fileType) {
+          case "css":
+            if (cssminFlag) {
+              fileContent = cssmin(fileContent)
+            }
+            break
+          
+          case "html":
+            if (htmlminFlag) {
+              fileContent = htmlmin(fileContent, htmlminOptions)
+            }
+            break
         }
 
         if (fs.existsSync(filePath)) {
